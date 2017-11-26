9d5c5c0d5a53df097bd9e9a6fb1ec99024da23b5
Merrick Kirby <makirby@outlook.com>
2017-11-26 15:26:15 +0000

    Add appcenter setup to android

--- android/app/src/main/java/com/rlmobile/MainApplication.java
+++ android/app/src/main/java/com/rlmobile/MainApplication.java
@@ -14,12 +14,10 @@ import java.util.List;
 
 public class MainApplication extends NavigationApplication {
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    FacebookSdk.sdkInitialize(getApplicationContext());
-    // If you want to use AppEventsLogger to log events.
-    AppEventsLogger.activateApp(this);
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
   }
 
   @Override
@@ -31,8 +29,10 @@ public class MainApplication extends NavigationApplication {
     // Add additional packages you require here
     // No need to add RnnPackage and MainReactPackage
     return Arrays.<ReactPackage>asList(
-      new FBSDKPackage(mCallbackManager)
-
+      new FBSDKPackage(mCallbackManager),
+       new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+      new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+      new AppCenterReactNativePackage(MainApplication.this)
     );
   }
 
@@ -41,10 +41,11 @@ public class MainApplication extends NavigationApplication {
     return getPackages();
   }
 
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
-
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    AppCenter.setLogLevel(Log.VERBOSE);
   }
 
 }
